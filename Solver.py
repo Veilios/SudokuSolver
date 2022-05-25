94363f099ed1efc9a1243122e51d4843c558af8c
Alan Mir <alanpaulphotos@gmail.com>
2022-05-24 22:54:51 -0400

    Created function for checking if a board is valid or not

--- Solver.py
+++ Solver.py
@@ -11,13 +11,38 @@ board = [
 ]
 
 
+def valid(bo, num, pos):
+
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check col
+    for i in range(len(bo[0])):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check sub grid
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
+
 def print_board(bo):
 
-    for i in range(len(bo)): # i represents the row
+    for i in range(len(bo)):  # i represents the row
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - -")
 
-        for j in range(len(bo[0])): # j represents the colomn
+        for j in range(len(bo[0])):  # j represents the colomn
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
 
@@ -31,4 +56,4 @@ def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
-                return (i, j) # row, col
+                return (i, j)  # row, col
