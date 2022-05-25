bbab2bbc592df30d5b48f8006c1541138844076f
Alan Mir <alanpaulphotos@gmail.com>
2022-05-24 23:03:54 -0400

    Created recursive function that solves the sudoku board with backtracking

--- Solver.py
+++ Solver.py
@@ -11,6 +11,26 @@ board = [
 ]
 
 
+def solve(bo):
+
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+            
+            bo[row][col] = 0
+    
+    return False
+
+
 def valid(bo, num, pos):
 
     # Check row
@@ -35,7 +55,6 @@ def valid(bo, num, pos):
     return True
 
 
-
 def print_board(bo):
 
     for i in range(len(bo)):  # i represents the row
@@ -57,3 +76,4 @@ def find_empty(bo):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
                 return (i, j)  # row, col
+    return None
