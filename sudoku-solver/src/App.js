375921d1016fab3011b9be8bcaa2d6f06b249790
Alan Mir <alanpaulphotos@gmail.com>
2022-05-25 15:04:08 -0400

    Created inputs to display sudoku board

--- sudoku-solver/src/App.js
+++ sudoku-solver/src/App.js
@@ -2,7 +2,121 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>Sudoku Solver</h1>
+    <>
+      <h1>Sudoku Solver</h1>
+
+      <div className='b' >
+
+        <div className='b_row1' >
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+        </div>
+
+        <div className='b_row2' >
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+        </div>
+
+        <div className='b_row3' >
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+        </div>
+
+        <div className='b_row4' >
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+        </div>
+
+        <div className='b_row5' >
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+        </div>
+
+        <div className='b_row6' >
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+        </div>
+
+        <div className='b_row7' >
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+        </div>
+
+        <div className='b_row8' >
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+        </div>
+
+        <div className='b_row9' >
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+          <input type="number" min="1" max="9" />
+        </div>
+
+      </div>
+    </>
   )
 }
 
