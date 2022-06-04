41af11332ad7a7177a11fe2d736f1b0401c495c0
Alan Mir <alanpaulphotos@gmail.com>
2022-06-03 23:57:39 -0400

    trying to redo the way I save the puzzle from sudoku api

--- sudoku-solver/src/App.js
+++ sudoku-solver/src/App.js
@@ -3,10 +3,58 @@ import React from "react";
 import SudokuBoard from "./components/SudokuBoard";
 import { SudokuProvider } from "./context/SudokuContext";
 
+// import generator from "sudoku";
+
 import "./App.scss";
 
 
 const App = (props) => {
+
+  // useEffect(() => {
+  //   let nBoard = generator.makepuzzle();
+  //   console.log("this: ", nBoard);
+
+  //   let sBoard = [
+  //     [],
+  //     [],
+  //     [],
+  //     [],
+  //     [],
+  //     [],
+  //     [],
+  //     [],
+  //     []
+  //   ];
+
+  //   // let cell = 0
+  //   // if (nBoard[nBoard.length - 1] === null) {
+  //   //   cell = 0
+  //   // } else {
+  //   //   cell = nBoard[nBoard.length - 1] 
+  //   // }
+
+  //   // console.log("cell", cell);  
+  //   let cell = 0
+
+  //   for (let i = 0; i < 9; i++) {
+  //     for (let j = 0; j < 9; j++) {
+  //       if (nBoard[nBoard - 1] === null) {
+  //         cell = 0;
+  //       } else {
+  //         cell = nBoard[nBoard - 1]
+  //       }
+
+  //       nBoard.pop();
+
+  //       sBoard[i] = [
+  //         ...sBoard[i],
+  //         nBoard[i][nBoard - 1] + 1
+  //       ]
+  //     }
+  //   };
+
+  //   console.log(sBoard);
+  // }, []);
 
 
   // const handleChange = (e) => {
---
+++ sudoku-solver/src/hooks/Valid.js
@@ -0,0 +1,7 @@
+export const Valid = (board, num, pos) => {
+    for (let i = 0; i < 9; i++) {
+        if (board[pos[0]][i] === num && pos[1] != i) {
+            
+        }
+    }
+};
